9315a29e7b819d06d31490ca0a820c05520498fc
Shivam <gupta.shivam2999@gmail.com>
2021-09-19 17:36:00 +0530

    First Commit

---
+++ lib/screens/login_success/components/body.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/default_button.dart';
+import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:shop_app/size_config.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: SizeConfig.screenHeight * 0.04),
+        Image.network(
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTca_P2qWbRzXD4qMIIZgOZEdNLEx-7X0GdQ&usqp=CAU",
+          height: SizeConfig.screenHeight * 0.4, //40%
+        ),
+        SizedBox(height: SizeConfig.screenHeight * 0.08),
+        Text(
+          "Login Success",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(30),
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        Spacer(),
+        SizedBox(
+          width: SizeConfig.screenWidth * 0.6,
+          child: DefaultButton(
+            text: "Back to home",
+            press: () {
+              Navigator.pushNamed(context, HomeScreen.routeName);
+            },
+          ),
+        ),
+        Spacer(),
+      ],
+    );
+  }
+}
